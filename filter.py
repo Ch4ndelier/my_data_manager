5e308231ee0f330adc8351a9b4154ecd38776c91
liujunyuan <ljyuan@bupt.edu.cn>
2020-11-30 21:16:56 +0800

    removing files in .gitignore

---
+++ filter.py
@@ -0,0 +1,28 @@
+import numpy as np
+import matplotlib.pyplot as plt
+Path = "./test.txt"
+data = np.loadtxt(Path, usecols=(0, 1, 2), dtype=int, delimiter=',')
+print(data)
+print(data.mean(0))
+mean = data.mean(0)
+out = []
+for line in data:
+    cnt = 0
+    for i in range(3):
+        if line[i] + 35 < mean[i]:
+            cnt += 1
+    if cnt > 1:
+        out.append(0)
+    else:
+        out.append(1)
+
+N = len(out)
+cnt = 0
+for i in range(N):
+    if i and out[i] > out[i - 1]:
+        cnt += 1
+        print(i)
+
+print('cnt', cnt)
+plt.plot(out)
+plt.show()
