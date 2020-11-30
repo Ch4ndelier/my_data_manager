2396f0f97cf19f7e9ec1f9b8fd3516839aa35b6f
liujunyuan <ljyuan@bupt.edu.cn>
2020-11-30 17:48:00 +0800

    del_timestamp

--- del_time.py
+++ del_time.py
@@ -1,9 +1,22 @@
-press_path = "./Datasets/miniIMU/20201127 10:26:58 /IMURecord/Press.txt"
-write_path = "./test.txt"
-f2 = open(write_path, 'w')
-with open(press_path, 'r+') as f:
+IMU_PATH = './Datasets/miniIMU/ori_paths.txt'
+
+
+def del_time(press_path, write_path):
+    f2 = open(write_path, 'w')
+    with open(press_path, 'r+') as f:
+        for line in f.readlines():
+            if len(line.split()) == 3:
+                line = line.split()[2] + '\n'
+            f2.write(line)
+    f2.close()
+
+
+data_path_list = []
+with open(IMU_PATH, 'r+') as f:
     for line in f.readlines():
-        if len(line.split()) == 3:
-            line = line.split()[2] + '\n'
-        f2.write(line)
-f2.close()
+        data_path_list.append(line[:-1].split('$'))
+
+# print(data_path_list)
+for data_path in data_path_list:
+    print(data_path)
+    del_time(data_path[1], data_path[2])
